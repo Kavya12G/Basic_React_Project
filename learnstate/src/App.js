ca7f11321b12d487ee1f66b0a022576b895dfba1
Kavya <kavyagidaveer@gmail.com>
2024-12-27 20:20:33 +0530

    props content

--- learnstate/src/App.js
+++ learnstate/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useState } from "react";
+// import logo from './assets/logo.svg'; 
+// import {useState} from 'react';
+import Counter from './Counter';
 
-function App() {
+export default function App() {
+  // let name = '';
+  const {name , setName} = useState('Neha')
+
+  const handleclick = ()=> {
+    // name = 'Akash';
+    // alert(name);
+    setName('Akash');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>Hello {name}</h1>
+      <button onClick={handleclick}>Click</button> */}
+      <Counter />
     </div>
-  );
+  )
 }
-
-export default App;
